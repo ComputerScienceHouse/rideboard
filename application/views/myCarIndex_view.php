808021acb9588e061fa99a9c030da879f5243cfa
Sean McGary <sean.mcgary@gmail.com>
2011-03-09 21:45:30 -0500

    adding stuff...

--- application/views/myCarIndex_view.php
+++ application/views/myCarIndex_view.php
@@ -1,12 +1,18 @@
 <div id="main-with-left">
-<?php
-    if($exists)
-    {
-        echo $view_vehicle;
-    }
-    else
-    {
-        echo $create;
-    }
-?>
+    <div class="dicks">
+    <?php
+        if($exists)
+        {
+            echo $view_vehicle;
+        }
+        else
+        {
+            echo $create;
+        }
+    ?>
+    </div>
+    <br>
+    <div class="dicks">
+        <h1>Manage VIP's</h1>
+    </div>
 </div>
